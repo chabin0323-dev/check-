672530435a7b3d69c794fab77615aae0f6563d7c
chabin0323-dev <chabin0323@gmail.com>
2026-04-09 22:01:03 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,16 @@
+export const metadata = {
+  title: "App",
+  description: "My App",
+};
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
