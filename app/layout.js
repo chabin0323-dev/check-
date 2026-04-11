0cfdace35b1cda52539a1b7e547e7d668fcb57ba
chabin0323-dev <chabin0323@gmail.com>
2026-04-11 11:00:51 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
 export default function RootLayout({ children }) {
   return (
-    <html lang="ja">
-      <body style={{ margin: 0 }}>{children}</body>
-    </html>
-  );
+    <html lang="ja"><body style={{ margin: 0 }}>{children}</body></html>
+  )
 }
