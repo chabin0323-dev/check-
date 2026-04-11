c164d3493de695cc75f1a41d411ba466f1230c21
chabin0323-dev <chabin0323@gmail.com>
2026-04-11 10:17:21 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, // エラーを無視してビルド
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // エラーを無視してビルド
   },
 }
 module.exports = nextConfig
