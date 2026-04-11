22b62eed616e949650e30acd21751bd412509878
chabin0323-dev <chabin0323@gmail.com>
2026-04-11 10:04:02 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
           二人の未来が具体的に見える！<br />
           今だけ開かれる特別価格の扉！
         </h2>
-        <a href="https://www.paypal.com/ncp/payment/PPGD76J8JB8ZQ" target="_blank" rel="noreferrer" style={{ display: 'block', outline: 'none' }}>
-          <img src="/image_7.png" style={{ width: '90%', maxWidth: '380px', cursor: 'pointer' }} alt="黄金のボタン" />
+        <a href="https://www.paypal.com/ncp/payment/PPGD76J8JB8ZQ" target="_blank" rel="noreferrer">
+          <img src="/image_7.png" style={{ width: '90%', maxWidth: '380px', cursor: 'pointer' }} alt="button" />
         </a>
         <p style={{ fontWeight: 'bold', fontSize: '26px', color: '#ff4da6', margin: '25px 0' }}>使い放題 780円</p>
         <div style={{ background: 'white', padding: '15px', borderRadius: '12px', marginTop: '20px' }}>
